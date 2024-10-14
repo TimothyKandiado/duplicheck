7cf5b2492c969c66c5b858f51e56c580aa6baa8b
TimothyKandiado <kandiadot@gmail.com>
2024-10-14 16:44:32 +0200

    Added goroutines for calculating the hash

--- hashing.go
+++ hashing.go
@@ -4,24 +4,77 @@ import (
 	"crypto/sha1"
 	"fmt"
 	"os"
+	"runtime"
 )
 
-type HashCode [20]byte
+const hashCodeLength = 20
+const channelBuffer = 100
+
+type HashCode [hashCodeLength]byte
 
 // goes through the 2D array of files with similar sizes,
 // calculates their hash, then groups the files with the same hash
 func findDuplicateFilesByHash(sameSizeFiles []FileList) []FileList {
 	duplicatesByHash := make([]FileList, 0)
 
+	filepathChannel := make(chan string, channelBuffer)
+	computedHashChannel := make(chan struct {
+		HashCode
+		string
+	}, channelBuffer)
+
+	defer close(filepathChannel)
+	defer close(computedHashChannel)
+
+	nullHash := HashCode(make([]byte, hashCodeLength))
+
+	// lambda function for computing hash value
+	hashComputer := func(filepath chan string, computedHash chan<- struct {
+		HashCode
+		string
+	}) {
+		for filePath := range filepath {
+			hashCode, err := hashFile(filePath)
+
+			if err != nil {
+				hashCode = nullHash
+				filePath = err.Error()
+			}
+
+			computedHash <- struct {
+				HashCode
+				string
+			}{hashCode, filePath}
+		}
+	}
+
+	// create go routines
+	numCpus := runtime.NumCPU()
+
+	if numCpus > 1 {
+		numCpus = numCpus - 1
+	}
+
+	for i := 0; i < numCpus; i++ {
+		go hashComputer(filepathChannel, computedHashChannel)
+	}
+
 	for _, files := range sameSizeFiles {
 		hashDuplicateWithSameSize := make(map[HashCode]FileList)
 
-		// group files by their hash value
+		// compute hashcode and store it in map
 		for _, filepath := range files {
-			hash, err := hashFile(filepath)
+			filepathChannel <- filepath
+		}
 
-			if err != nil {
-				fmt.Println(err)
+		for i := 0; i < len(files); i++ {
+			computedHash := <-computedHashChannel
+
+			hash := computedHash.HashCode
+			filepath := computedHash.string
+
+			if hash == nullHash {
+				fmt.Println("Error computing hash ", filepath)
 				continue
 			}
 
@@ -53,7 +106,7 @@ func hashFile(path string) (HashCode, error) {
 	data, err := os.ReadFile(path)
 
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error: could not compute hashcode: ", err)
 		return [20]byte{}, err
 	}
 
